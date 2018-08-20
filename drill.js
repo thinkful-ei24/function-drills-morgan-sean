bfa1977a1829c5c80923e6df73d7bd39e58c2a85
Morgan Newman <mpnewman95@gmail.com>
2018-08-20 11:41:01 -0700

    Refactored createGreeting()

--- drill.js
+++ drill.js
@@ -4,9 +4,13 @@ function printName(name, age) {
 
 printName("Sean", 21);
 
+const getYearOfBirth = function(age) {
+  return (new Date()).getFullYear() - age;
+};
+
 const createGreeting = function(name, age) {
-  const birthYear = 2018 - age;
+  const birthYear = getYearOfBirth(age);
   return `Hi, my name is ${name} and I'm ${age} years old.\nI was born in ${birthYear}`;
 };
 
-console.log(createGreeting("Morgan", 22))
+console.log(createGreeting("Morgan", 22));
