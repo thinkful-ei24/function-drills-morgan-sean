508a4a6901eb7c84e5a847fd48d24b3b17bf9faa
Morgan Newman <mpnewman95@gmail.com>
2018-08-20 11:52:44 -0700

    Added error handling for createGreeting()

--- drill.js
+++ drill.js
@@ -6,12 +6,15 @@ printName("Sean", 21);
 
 const getYearOfBirth = function(age) {
   if(age < 0) {
-    throw new Error("Age cannot be negative")
+    throw new Error("Age cannot be negative");
   }
   return (new Date()).getFullYear() - age;
 };
 
 const createGreeting = function(name, age) {
+  if (name === undefined || age === undefined) {
+    throw new Error('Arguments not valid');
+  }
   const birthYear = getYearOfBirth(age);
   return `Hi, my name is ${name} and I'm ${age} years old.\nI was born in ${birthYear}`;
 };
